81334df006c0752e1d14c95a5ffb8aef77dd69be
林宛慧 <aa0960757329@gmail.com>
2021-07-11 23:10:07 +0800

    0711_ 評選機制完成、top1000 篩選功能未完成、名單未提供

--- assets/js/anchor.js
+++ assets/js/anchor.js
@@ -69,7 +69,6 @@ $(function(){
 });
 
 
-
 $(function(){
   // go top
   $(window).scroll(function() {
@@ -81,10 +80,10 @@ $(function(){
     }
   });
 
-    $(".gotop").click(function () {
-        $("html,body").animate({ scrollTop: 0 }, 900);
-        return false;
-    });
+  $(".gotop").click(function () {
+      $("html,body").animate({ scrollTop: 0 }, 900);
+      return false;
+  });
 
   // go down
   $("#godown").click(function () {
@@ -94,4 +93,37 @@ $(function(){
 
 });
 
-
+// top1000 FAQ
+$(function(){
+  $('.answer').hide(); 
+  $(".question--1").click(function () {
+    $(".answer--1").slideToggle("slow");
+    $(".answer--4,.answer--2,.answer--3,.answer--5").slideUp("slow"); 
+    $(".btn_toggle--1 span:nth-child(2)").toggle(); 
+    $(".btn_toggle--2 span:nth-child(2),.btn_toggle--3 span:nth-child(2),.btn_toggle--4 span:nth-child(2),.btn_toggle--5 span:nth-child(2)").show(); 
+  });
+  $(".question--2").click(function () {
+    $(".answer--2").slideToggle("slow");
+    $(".answer--1,.answer--4,.answer--3,.answer--5").slideUp("slow"); 
+    $(".btn_toggle--2 span:nth-child(2)").toggle();
+    $(".btn_toggle--1 span:nth-child(2),.btn_toggle--3 span:nth-child(2),.btn_toggle--4 span:nth-child(2),.btn_toggle--5 span:nth-child(2)").show(); 
+  });
+  $(".question--3").click(function () {
+    $(".answer--3").slideToggle("slow");
+    $(".answer--1,.answer--2,.answer--4,.answer--5").slideUp("slow"); 
+    $(".btn_toggle--3 span:nth-child(2)").toggle(); 
+    $(".btn_toggle--2 span:nth-child(2),.btn_toggle--1 span:nth-child(2),.btn_toggle--4 span:nth-child(2),.btn_toggle--5 span:nth-child(2)").show(); 
+  });
+  $(".question--4").click(function () {
+    $(".answer--4").slideToggle("slow");
+    $(".answer--1,.answer--2,.answer--3,.answer--5").slideUp("slow"); 
+    $(".btn_toggle--4 span:nth-child(2)").toggle(); 
+    $(".btn_toggle--2 span:nth-child(2),.btn_toggle--3 span:nth-child(2),.btn_toggle--1 span:nth-child(2),.btn_toggle--5 span:nth-child(2)").show(); 
+  });
+  $(".question--5").click(function () {
+    $(".answer--5").slideToggle("slow");
+    $(".answer--1,.answer--2,.answer--3,.answer--4").slideUp("slow"); 
+    $(".btn_toggle--5 span:nth-child(2)").toggle(); 
+    $(".btn_toggle--2 span:nth-child(2),.btn_toggle--3 span:nth-child(2),.btn_toggle--4 span:nth-child(2),.btn_toggle--1 span:nth-child(2)").show(); 
+  });
+});
